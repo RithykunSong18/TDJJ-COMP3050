0d92032bd4185e159e17b4ebc504c85db5cf8aba
stalebiscuit <161665600+stalebiscuit@users.noreply.github.com>
2026-04-29 06:08:33 +0000

    Testing this branch

--- main/server/Test.java
+++ main/server/Test.java
@@ -5,7 +5,8 @@ public class Test {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/info", new MyHandler()); //change myhandler to infohandler
+        server.createContext("/move", new MyHandler()); //change myhandler to movehandler
         server.setExecutor(null); // creates a default executor
         server.start();
     }
